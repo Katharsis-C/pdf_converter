2e7ec243c65f5b2e4344f2f309548ef8c641d312
Lumix <sl-xp7@yandex.com>
2020-05-26 01:07:37 +0800

    init

---
+++ routes/index.js
@@ -0,0 +1,69 @@
+const Koa = require('koa')
+const app = new Koa()
+const router = require('koa-router')()
+const multer = require('@koa/multer')
+const render = require('koa-ejs')
+
+const fs = require('fs')
+const PDFExtract = require('pdf.js-extract').PDFExtract
+
+const pdfExtract = new PDFExtract()
+
+const storage = multer.diskStorage({
+    //定义文件保存路径
+    destination: function (req, file, cb) {
+        console.log(file)
+        cb(null, './pdf/')
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname)
+    },
+})
+
+const upload = multer({ storage: storage })
+
+router.post('/pdf', upload.single('file'), async (ctx, next) => {
+    let { password } = ctx.request.body
+    let options = null
+    if (!!password) {
+        options = { disableCombineTextItems: true, password: password }
+    } else {
+        options = { disableCombineTextItems: true }
+    }
+    try {
+        let file = ctx.request.file
+        // console.log(file)
+        // console.log(ctx.request.file.path)
+        let path = `./${file.path}`
+        // console.log(buffer)
+        let str = []
+        let data = await pdfExtract.extract(path, options).then((data) => data)
+        for (const pages of data.pages) {
+            for (const content of pages.content) {
+                str.push(content.str)
+            }
+        }
+        return next().then(async () => {
+            fs.unlink(`./pdf/${file.originalname}`, (err) => {
+                if (err) {
+                    console.log(err)
+                } else {
+                    console.log('done')
+                }
+            })
+            await ctx.render('index', {data: str})
+            // console.log(str)
+        })
+    } catch (error) {
+        console.log(error)
+        ctx.response.body = {
+            msg: 'error',
+        }
+    }
+})
+
+router.get('/pdf', async (ctx, next) => {
+    await ctx.render('index', {})
+})
+
+module.exports = router
