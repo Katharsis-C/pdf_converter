ae04b65dd48075c86b5e8f0ecd074f92422c7af7
zuiko <sl-xp7@yandex.com>
2021-01-11 17:55:45 +0800

    function type

---
+++ function.js
@@ -0,0 +1,44 @@
+//imort fs
+const fs = require('fs')
+//import extract method
+const PDFExtract = require('pdf.js-extract').PDFExtract
+//init method
+const pdfExtract = new PDFExtract()
+
+
+async function PDF_convert(path = '', password = null) {
+  //abort when file is not pdf
+  if (path.indexOf('.pdf') == -1) {
+    return ''
+  }
+
+  let options = { disableCombineTextItems: true }
+  let str = []
+
+  if (!!password) {
+    Object.assign(options, { password: password })
+  }
+
+  let data = await pdfExtract
+    .extract(path, options)
+    .then((data) => {
+      for (const pages of data.pages) {
+        for (const content of pages.content) {
+          str.push(content.str)
+        }
+      }
+      return data
+    })
+    .catch((err) => {
+      if (err.name == 'PasswordException') {
+        return 'Password error'
+      }
+    })
+
+  console.log(28, { str, data, pw:options.password })
+  return str
+}
+
+// PDF_convert(`C:\\Users\\Saki\\Downloads\\1.pdf`, '')
+// PDF_convert(`C:\\Users\\Saki\\Downloads\\2.pdf`, '5316DN92')
+// PDF_convert(`C:\\Users\\Saki\\Downloads\\2.pdf`, '')
--- routes/index.js
+++ routes/index.js
@@ -10,62 +10,62 @@ const PDFExtract = require('pdf.js-extract').PDFExtract
 const pdfExtract = new PDFExtract()
 
 const storage = multer.diskStorage({
-    //定义文件保存路径
-    destination: function (req, file, cb) {
-        console.log(file)
-        cb(null, './pdf/')
-    },
-    filename: function (req, file, cb) {
-        cb(null, file.originalname)
-    },
+  //定义文件保存路径
+  destination: function (req, file, cb) {
+    console.log(file)
+    cb(null, './pdf/')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname)
+  },
 })
 
 const upload = multer({ storage: storage })
 
 router.post('/pdf', upload.single('file'), async (ctx, next) => {
-    let { password } = ctx.request.body
-    let options = null
-    if (!!password) {
-        options = { disableCombineTextItems: true, password: password }
-    } else {
-        options = { disableCombineTextItems: true }
+  let { password } = ctx.request.body
+  let options = null
+  if (!!password) {
+    options = { disableCombineTextItems: true, password: password }
+  } else {
+    options = { disableCombineTextItems: true }
+  }
+  try {
+    let file = ctx.request.file
+    // console.log(file)
+    // console.log(ctx.request.file.path)
+    let path = `./${file.path}`
+    // console.log(buffer)
+    let str = []
+    let data = await pdfExtract.extract(path, options).then((data) => data)
+    for (const pages of data.pages) {
+      for (const content of pages.content) {
+        str.push(content.str)
+      }
     }
-    try {
-        let file = ctx.request.file
-        // console.log(file)
-        // console.log(ctx.request.file.path)
-        let path = `./${file.path}`
-        // console.log(buffer)
-        let str = []
-        let data = await pdfExtract.extract(path, options).then((data) => data)
-        for (const pages of data.pages) {
-            for (const content of pages.content) {
-                str.push(content.str)
-            }
+    return next().then(async () => {
+      fs.unlink(`./pdf/${file.originalname}`, (err) => {
+        if (err) {
+          console.log(err)
+        } else {
+          console.log('done')
         }
-        return next().then(async () => {
-            fs.unlink(`./pdf/${file.originalname}`, (err) => {
-                if (err) {
-                    console.log(err)
-                } else {
-                    console.log('done')
-                }
-            })
-            await ctx.render('index', {data: str})
-            // console.log(str)
-        })
-    } catch (error) {
-        ctx.response.body = {
-            code: -1,
-            msg: error.message,
-        }
+      })
+      await ctx.render('index', { data: str })
+      // console.log(str)
+    })
+  } catch (error) {
+    ctx.response.body = {
+      code: -1,
+      msg: error.message,
     }
+  }
 })
 
 router.get('/pdf', async (ctx, next) => {
   ctx.response.body = {
     code: 200,
-    msg: 'test'
+    msg: 'test',
   }
 })
 
